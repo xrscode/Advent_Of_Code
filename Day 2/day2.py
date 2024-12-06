0a5531161846d0f96f7c265a69557273263fb238
M2 <m2@M2s-Mac-mini.localdomain>
2024-12-06 09:57:14 +0000

    .

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,40 +1,80 @@
-def day2():
+# Function to access data:
+def get_data(location):
     data = []
 
     # Open data and append to list:
-    with open('./data.txt', 'r') as d:
+    with open(f'{location}', 'r') as d:
         for line in d:
             lst = [int(x) for x in line.strip().split(' ')]
             data.append(lst)
-
-    safe = 0
-    unsafe = 0
-
-    # Iterate through data:
-    for x in data:
-        diff = []
-        for i, y in enumerate(x):
-            if i + 1 == len(x):
-                continue
-            else:
-                diff.append(y - x[i + 1])
+    return data
         
-        # Check for 0
-        if 0 in diff:
-            unsafe += 1
-            continue
-        
-        # Check for values greater than 3 and less than -3
-        if any(x > 3 for x in diff) or any(x < -3 for x in diff):
-            unsafe += 1
-            continue
-      
-        # Check values are either all positive or all neg:
-        if all(x > 0 for x in diff) or all(x < 0 for x in diff):
-            safe += 1
-            continue
+
+
+# Function to check list:
+def checkList(list):
+    diff = []
+    # Iterate through each item: e.g. 7, 6, 4, 2, 1
+    for i, x in enumerate(list):
+        # Determine last position: e.g. 1
+        if i == len(list) - 1:
+            pass
+        else:
+            temp_num = (x - list[i+1]) * -1
+            diff.append(temp_num)
     
-    print(f'Safe: {safe}, Unsafe: {unsafe}')
+    # Analyse difference list:
+    # Levels must ALL be increasing OR decreasing
+    # Levels must differ by at least one and at most three.
+    increase = all(x > 0 and x <= 3 for x in diff)
+    decrease = all(x < 0 and x >= -3 for x in diff)
+    
+    if increase or decrease:
+        return True
+    
+    return False
+
+
+# Sample Data
+sample_data = get_data('./sample.txt')
+# Live Data
+live_data = get_data('./data.txt')
+
+
+# Check data:
+def dampner(list):
+    problems = 0
+    success = 0
+
+    if not checkList(list):
+        print('Checking list: ', list)
+        for i, x in enumerate(list):
+            new_list = list[:i] + list[i+1:]
+            if not checkList(new_list):
+                problems += 1
+            else:
+                success += 1
+    if success > 0:
+        return True
+    else:
+        return False
+
+            
+safe = 0
+unsafe = 0
+
+# Iterate through data:
+for x in live_data:
+    if checkList(x):
+        safe += 1
+    else:
+       if dampner(x):
+           safe += 1
+       else:
+           unsafe += 1
+
+print('Safe is: ', safe)
+print('Unsafe is: ', unsafe)
 
         
 
@@ -42,4 +82,5 @@ def day2():
 
 
 
-day2()
+
+
