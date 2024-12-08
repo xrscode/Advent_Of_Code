2f7c2a93604a5e0048994b3e08de7ee4036d10c0
xrscode <xrs@icloud.com>
2024-12-08 17:26:08 +0000

    Day 6 Part 1 Complete

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,17 +1,15 @@
-sample = """....#.....
-.........#
-..........
-..#.......
-.......#..
-..........
-.#..^.....
-........#.
-#.........
-......#..."""
-data = [row for row in sample.split('\n')]
+# Function to access data:
+def get_data(location):
+    arr = []
+    # Open data and append to list:
+    with open(f'{location}', 'r') as d:
+        for i, x in enumerate(d):
+            arr.append(x)
+    
+    return arr
 
-# for line in data:
-#     print(line)
+# Read live data:
+data = get_data('./data.txt')
 
 pos = {'x': 0, 'y': 0}
 
@@ -27,66 +25,88 @@ for y, row in enumerate(data):
         if char in  chars:
             pos['x'] = x
             pos['y'] = y
-            print(f'Character found at co-ordinates: {pos}')
+            
             current = chars[chars.index(char)]
-            recorded_positions.append(pos)
-            # pos moving:
+            recorded_positions.append(pos.copy())  # Append a copy of `pos`
+            
+
+
+
             while 0 < pos['y'] < len(data) - 1 and 0 < pos['x'] < len(row):
                 # UP:
                 if current == '^':
-                    print(recorded_positions)
+                   
                     # Next position: x + 0, y - 1:
                     new_y = pos['y'] - 1
                     # Check:
                     if data[new_y][pos['x']] == '#':
-                        print('Block found!  Turning right!')
+                        
                         current = '>'
-                        print(f'Current position is {pos}, Current character is: {current}')
+                       
                     else:
-                        print('Moving forward!')
                         pos['y'] = new_y
-                        count += 1
-                    
+                       
+                        if pos not in recorded_positions:
+                            recorded_positions.append(pos.copy())  # Append a copy of `pos`
+                
+                
+                
+                
+                # RIGHT
                 if current == '>':
-                    print(recorded_positions)
+                   
                     # Next position:  x + 1, y + 0
                     new_x = pos['x'] + 1
                     # Check:
                     if data[pos['y']][new_x] == '#':
-                        print('Block found!  Turning right!')
+                       
                         current = 'v'
-                        print(f'Current position is {pos}, Current character is: {current}')
+                       
                     else:
-                        count += 1
-                        print('Moving forward!')
                         pos['x'] = new_x
+                       
+                        if pos not in recorded_positions:
+                            recorded_positions.append(pos.copy())  # Append a copy of `pos`
+                
+                
+                
+                
+                # DOWN
                 if current == 'v':
-                    print(recorded_positions)
+                   
                     # Next position:  x + 0, y + 1
                     new_y = pos['y'] + 1
                     # Check:
                     if data[new_y][pos['x']] == '#':
-                        print('Block found!  Turning right!')
+                        
                         current = '<'
-                        print(f'Current position is {pos}, Current character is: {current}')
+                      
                     else:
-                        count += 1
-                        print('Moving forward!')
                         pos['y'] = new_y
+                       
+                        if pos not in recorded_positions:
+                            recorded_positions.append(pos.copy())  # Append a copy of `pos`
+                
+                
+                
+                # LEFT
                 if current == '<':
-                    print(recorded_positions)
+                   
                     # Next position:  x -1 1, y - 1
                     new_x = pos['x'] - 1
                     # Check:
                     if data[pos['y']][new_x] == '#':
-                        print('Block found!  Turning right!')
+                       
                         current = '^'
-                        print(f'Current position is {pos}, Current character is: {current}')
+                       
                     else:
-                        count += 1
-                        print('Moving forward!')
                         pos['x'] = new_x
-            print('Total count is: ', count)
+                       
+                        if pos not in recorded_positions:
+                            recorded_positions.append(pos.copy())  # Append a copy of `pos`
+
+            
+print('Total count is: ', len(recorded_positions))
                 
 
 
