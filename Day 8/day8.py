b40cae47230d9c2a7f69fd1368b75fe84cda8f2e
xrscode <xrs@icloud.com>
2024-12-12 12:18:03 +0000

    Partial day 8

--- Day 8/day8.py
+++ Day 8/day8.py
@@ -1,4 +1,5 @@
 import itertools
+import re
 
 # Function to access data:
 def get_data(location):
@@ -17,18 +18,21 @@ data = get_data('./data.txt')
 live_data = get_data('./data.txt')
 live_data.reverse()
 sample_data = get_data('./sample.txt')
+sample_two = get_data('./sample2.txt')
 
 # Diagram to help visualise concept:
 for i, line in enumerate(sample_data):
     print(i, line.strip())
 
 def antinode(data):
-    
+    # Pattern to match single lowercase or upper case letter or digit.
+    pattern = r'^[a-zA-Z0-9]$'
+
     # Create boundary check function:
     def boundary(x, y):
         # X must be less than length of row:
         if x >= 0 and x <= len(data[0]) - 1:
-            # Y must be less than lenght of data set:
+            # Y must be less than length of data set:
             if y >= 0 and y <= len(data) - 1:
                 return True
         return False
@@ -40,77 +44,115 @@ def antinode(data):
     # Iterate through data to find antennaes:
     for y, row in enumerate(data):
         for x, col in enumerate(row):
-            if data[y][x] == 'a' and data[y][x] != '#':
-                char = data[y][x]
-                print('Antennae found at: ', x, y)
+            char = data[y][x]
+
+            if re.match(pattern, char):
                 if char not in locations:
                     locations[char] = [[x, y]]
                 else:
                     locations[char].append([x, y])
+
     
     # Iterate through atennae locations:
     for antennae in locations:
         # Generate list of all possible combinations:
-        combinations = list(itertools.product(locations[antennae], repeat=len(locations[antennae])))
+        combinations = list(itertools.combinations(locations[antennae], 2))
+
         
        
        # Iterate through pairs of combinations to make comparisons:
         for i, x in enumerate(combinations):
 
-            # Check not comparing same values:
-            if x[0] != x[1]:
-                # Establish Antenna Locations:
-                ant_1 = [x[0][0], x[0][1]]
-                ant_2 = [x[1][0], x[1][1]]
-                
-                # Sometimes some values will be the same.
-                if ant_1[0] != ant_2[0] and ant_1[1] != ant_2[1]:
-                    x_dis = ant_1[0] - ant_2[0]
-                    y_dis = ant_1[1] - ant_2[1]
-                    
-                    # Determine largest/smallest x value. 
-                    if x_dis <= 0:
-                        lrgX = ant_2[0]
-                        smlX = ant_1[0]
-                    else:
-                        lrgX = ant_1[0]
-                        smlX = ant_2[0]
-                    
-                    # Determine largest/smallest y value.
-                    if y_dis <= 0:
-                        lrgY = ant_2[1]
-                        smlY = ant_1[1]
-                    else:
-                        lrgY = ant_1[1]
-                        smlY = ant_2[1]
+            
+            # Establish Antenna Locations:
+            ant_1 = [x[0][0], x[0][1]]
+            ant_2 = [x[1][0], x[1][1]]
+            
+            
+            # Determine distances:
+            x_dis = ant_1[0] - ant_2[0]
+            y_dis = ant_1[1] - ant_2[1]
+            
 
-                    # Make displacement positive if negative.
-                    x_dis = x_dis if x_dis >= 0 else x_dis * - 1
-                    y_dis = y_dis if y_dis >= 0 else y_dis * - 1
-
-                    # Find new Antinode values:
-                    ant_n_one = [lrgX + x_dis, lrgY + y_dis]
-                    ant_n_two = [smlX - x_dis, smlY - y_dis]
-                 
-                    
-                    if boundary(ant_n_one[0], ant_n_one[1]) and ant_n_one not in antinodes:
-                            antinodes.append(ant_n_one)
-                 
-                   
-                    if boundary(ant_n_two[0], ant_n_two[1]) and ant_n_two not in antinodes:
-                            antinodes.append(ant_n_two)
+            # Determine largest/smallest x value. 
+            if x_dis <= 0:
+                lrgX = ant_2[0]
+                smlX = ant_1[0]
+            else:
+                lrgX = ant_1[0]
+                smlX = ant_2[0]
 
 
-               
-        
-                
-                # Check Vertical: y 0, x 1
-                # Check Horizontal: y1, x 0œ
+            # Determine largest/smallest y value.
+            if y_dis <= 0:
+                lrgY = ant_2[1]
+                smlY = ant_1[1]
+            else:
+                lrgY = ant_1[1]
+                smlY = ant_2[1]
+
+
+
+            # Make displacement positive if negative.
+            x_dis = x_dis if x_dis >= 0 else x_dis * - 1
+            y_dis = y_dis if y_dis >= 0 else y_dis * - 1
+
+
+            
+            # Start calculating antinodes:
+            # First calculate next x value:
+            if ant_1[0] == lrgX:
+                v1 = ant_1[0] + x_dis
+            else:
+                v1 = ant_1[0] - x_dis
+            
+            # Calculate next y value:
+            if ant_1[1] == lrgY:
+                v2 = ant_1[1] + y_dis
+            else:
+                v2 = ant_1[1] - y_dis
+            
+            if ant_2[0] == lrgX:
+                v3 = ant_2[0] + x_dis
+            else:
+                v3 = ant_2[0] - x_dis
+            if ant_2[1] == lrgY:
+                v4 = ant_2[1] + y_dis
+            else:
+                v4 = ant_2[1] - y_dis
+
+
+            # Find new Antinode values:
+            ant_n_one = [v1, v2]
+            ant_n_two = [v3, v4]
+
+
+            # Check that antinodes are within boundary:
+            if boundary(ant_n_one[0], ant_n_one[1]) and ant_n_one not in antinodes:
+                    antinodes.append(ant_n_one)
+            if boundary(ant_n_two[0], ant_n_two[1]) and ant_n_two not in antinodes:
+                    antinodes.append(ant_n_two)
+
             
            
-    print(antinodes)
-    return antinodes
+    return len(antinodes)
 
 
-# Run function with sample data:
-antinode(sample_data)
+print(f'The answer to part one is: {antinode(data)}')
+
+
+string = """##....#....#
+.#.#....0...
+..#.#0....#.
+..##...0....
+....0....#..
+.#...#A....#
+...#..#.....
+#....#.#....
+..#.....A...
+....#....A..
+.#........#.
+...#......##
+""".split('\n')
+antinode(string)
+
